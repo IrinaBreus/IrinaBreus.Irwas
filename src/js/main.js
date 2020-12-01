604d94dbb26d73fd3340ce411b5e8b6e0ea3d1e1
Irina <irabreus@gmail.com>
2020-12-01 15:19:00 +0300

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import modal from './modules/modal';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 document.addEventListener('DOMContentLoaded', () => {
 'use strict';
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         form: 0,
         type: "tree"
     };
+    let deadline = '2020-12-18';
 
     changeModalState(modalState);
     modal();
@@ -18,4 +20,5 @@ document.addEventListener('DOMContentLoaded', () => {
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
     forms(modalState);
+    timer('.container1', deadline);
 });
